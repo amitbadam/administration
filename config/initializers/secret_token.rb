36e077e6671363912ee0a4f947092899cf7f55f6
amitbadam <amitbadam@gmail.com>
2013-12-05 23:24:06 +0530

    Admin interface

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Administration::Application.config.secret_token = '87a3fda3742b62109e0b97470bdfeaa778389e79fd157c68404638cb7afa56700709ad72bd41cc7e5558b82cfff63f9ed5cf84528e0832437860d6404cbc6c0c'
